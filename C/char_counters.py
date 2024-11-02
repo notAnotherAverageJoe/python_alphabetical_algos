1a871cf4aa4a7e6f6bbb6b419b265c16c2e4362a
joseph skokan <joeskokan20@gmail.com>
2024-11-02 13:12:03 -0400

    small fix

--- C/char_counters.py
+++ C/char_counters.py
@@ -1,28 +1,30 @@
 # make a function that founds frequencies of words within a string
 
+
 def freq_counter(str):
     counter = {}
-    
-    words = str.split(' ')
-    
+
+    words = str.split(" ")
+
     for i in range(len(words)):
-        counter[words[i]]=  counter.get(words[i],0) + 1
-        
+        counter[words[i]] = counter.get(words[i], 0) + 1
+
     return counter
-    
-    
 
 
 def freq_counter2(str):
     counter = {}
-    words = str.split(' ')
-    
+    words = str.split(" ")
+
     for word in words:
         counter[word] = counter.get(word, 0) + 1
-        
-        
+
     return counter
-sentence = "this is the testing line that will check the frequency counts within the sentence"
+
+
+sentence = (
+    "this is the testing line that will check the frequency counts within the sentence"
+)
 
 print(freq_counter(sentence))
 print("----------------------------------------")
